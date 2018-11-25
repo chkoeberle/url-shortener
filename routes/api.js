17b72cb82d53cb1387dbebb37853d9e15ebe714d
ckoeberle <chistian.koeberle@zalando.de>
2018-11-25 17:24:55 +0100

    #4 generate slug

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 
 module.exports = app => {
-    app.get('/', async(req,res) =>{
-        res.send('url shortener')
+    app.post('/api/url', async (req, res) => {
+        const targetUrl = req.body.url;
+        const url = await db.createUrlEntry(new URL(targetUrl, 'ABCDE'));
+        res.contentType('application/json').send(url);
     });
 };
